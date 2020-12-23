676526509b2206f6eec9685e62548f7e18cdd161
sky101a <65938943+sky101a@users.noreply.github.com>
2020-12-23 22:13:58 +0800

    Update 1907.sh

--- 1907.sh
+++ 1907.sh
@@ -21,8 +21,8 @@ sed -i 's/192.168.1.1/192.168.31.190/g' package/base-files/files/bin/config_gene
 
 #添加自定义插件链接（自己想要什么就github里面搜索然后添加）
 #git clone -b 18.06 https://github.com/garypang13/luci-theme-edge package/luci-theme-edge  #主题-edge-动态登陆界面
-git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom  #透明主题
-git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash  #openclash出国软件
+#git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom  #透明主题
+#git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-openclash  #openclash出国软件
 #git clone https://github.com/frainzy1477/luci-app-clash package/luci-app-clash  #clash出国软件
 #git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan  #微信推送
 #git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns  #smartdns DNS加速
@@ -30,17 +30,17 @@ git clone -b master https://github.com/vernesong/OpenClash.git package/luci-app-
 
 
 #lede的ShadowSocksR Plus+出国软件（19.07源码自带passwall出国软件）
-git clone https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2 package/redsocks2
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/tcpping package/tcpping
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/microsocks package/microsocks
+#git clone https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2 package/redsocks2
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/tcpping package/tcpping
+#svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/microsocks package/microsocks
 
 
-git clone https://github.com/jerrykuku/node-request.git package/node-request  #京东签到依赖
-git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus  #luci-app-jd-dailybonus[京东签到]
+#git clone https://github.com/jerrykuku/node-request.git package/node-request  #京东签到依赖
+#git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus  #luci-app-jd-dailybonus[京东签到]
 
 
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon  #argon-主题
+#git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon  #argon-主题
 #全新的[argon-主题]登录界面,图片背景跟随Bing.com，每天自动切换
 #增加可自定义登录背景功能，请自行将文件上传到/www/luci-static/argon/background 目录下，支持jpg png gif格式图片，主题将会优先显示自定义背景，多个背景为随机显示，系统默认依然为从bing获取
 #增加了可以强制锁定暗色模式的功能，如果需要，请登录ssh 输入：touch /etc/dark 即可开启，关闭请输入：rm -rf /etc/dark，关闭后颜色模式为跟随系统
